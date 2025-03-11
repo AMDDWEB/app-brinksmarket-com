c2c58e6fc475591693ef9704735d8fee92474b8c
ctab30 <88698939+ctab30@users.noreply.github.com>
2025-03-11 11:17:52 -0500

    Re Clone for New fixes
    Used ACDC to reclone So all new fixes were applied.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.allianceharshaadvertising.marketplacedev',
-  appName: 'Market Place',
+  appId: 'com.a2adv.brinksmarket',
+  appName: 'Brinks Market',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
